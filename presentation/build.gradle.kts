2dc3f01bba09f83cc09fbca06dc525eca6c04602
Mohamed <m.3.abdelfadeel@gmail.com>
2025-06-30 00:39:30 +0300

    add design system dependency to presentation module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 
 dependencies {
 
+    implementation (project(":design_system"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
