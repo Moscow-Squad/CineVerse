61009f53da6b1368d08b540b352d85b0e0835580
Israa Mohamed <e.mohamed@shabek.com>
2025-06-29 22:44:34 +0300

    Revert "Revert "integrate with firebase analytics, crashalitcs, app performance and a…""
    This reverts commit d789b23b95bf90957884916f4638ed16bd1b4b6a.

--- app/src/main/java/com/moscow/cineverse/MainActivity.kt
+++ app/src/main/java/com/moscow/cineverse/MainActivity.kt
@@ -6,17 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.moscow.cineverse.ui.theme.CineVerseTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        analytics = Firebase.analytics
+
         enableEdgeToEdge()
+
         setContent {
             CineVerseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -32,10 +43,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Button(onClick = {
+        throw RuntimeException("Test Crash") // Force a crash
+
+    }) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
