73e190c8d5a46467e61dd054888db431d0bf1678
AdelTamer <adeltammer@gmail.com>
2025-06-30 03:34:31 +0300

    feat: create separate file for manrope font

--- design_system/src/main/java/typography/DefaultTextStyle.kt
+++ design_system/src/main/java/typography/DefaultTextStyle.kt
@@ -2,18 +2,8 @@ package typography
 
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.design_system.R
-
-val Manrope = FontFamily(
-    Font(R.font.manrope_regular, FontWeight.Normal),
-    Font(R.font.manrope_medium, FontWeight.Medium),
-    Font(R.font.manrope_semi_bold, FontWeight.SemiBold),
-    Font(R.font.manrope_bold, FontWeight.Bold)
-)
 
 val DefaultTextStyle = CineVerseTextStyle(
     display = TextStyle(
