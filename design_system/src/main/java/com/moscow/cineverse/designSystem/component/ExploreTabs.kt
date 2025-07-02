9063dee577e1e9d50d77350425dacdafde45ddb3
mohamed_Galal <az.galal666@gmail.com>
2025-07-02 17:55:02 +0300

    refactor: change indicator color and change tabsToShow to be responsive

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/ExploreTabs.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/ExploreTabs.kt
@@ -1,11 +1,15 @@
 package com.moscow.cineverse.designSystem.component
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,7 +19,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.moscow.cineverse.designSystem.theme.CineVerseTheme
 import com.moscow.cineverse.designSystem.theme.Theme
+import com.moscow.cineverse.designSystem.theme.ThemeState
 import kotlinx.coroutines.launch
 
 @Composable
@@ -25,17 +31,26 @@ fun ExploreTabs(
     showAllTabs: Boolean = true,
     selectedContentColor: Color = Theme.colors.brand.primary,
     unselectedContentColor: Color = Theme.colors.shade.tertiary,
-    onTabSelected: (Int) -> Unit = {}
 ) {
     val scope = rememberCoroutineScope()
-    val tabsToShow = remember {
+    val tabsToShow = remember(showAllTabs) {
         if (showAllTabs) ExploreTabsPages.entries
         else ExploreTabsPages.entries.take(2)
     }
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
-        TabRow(selectedTabIndex = pagerState.currentPage) {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            contentColor = selectedContentColor,
+            indicator = { tabPositions ->
+                TabRowDefaults.SecondaryIndicator(
+                    Modifier
+                        .tabIndicatorOffset(tabPositions[pagerState.currentPage]),
+                    color = selectedContentColor
+                )
+            },
+        ) {
             tabsToShow.forEachIndexed { index, currentTab ->
                 Tab(
                     selected = pagerState.currentPage == index,
@@ -53,15 +68,24 @@ fun ExploreTabs(
     }
 }
 
-@Preview
+@Preview(
+    name = "Tabs Dark Mode",
+    uiMode = UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Preview(
+    name = "Tabs Light Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
 @Composable
 private fun ExploreTabsPreview() {
-    val pagerState = rememberPagerState(pageCount = { ExploreTabsPages.entries.size })
-    val showAllTabs by remember { mutableStateOf(false) }
+    CineVerseTheme {
+        val pagerState = rememberPagerState(pageCount = { ExploreTabsPages.entries.size })
+        val showAllTabs by remember { mutableStateOf(true) }
 
-    ExploreTabs(
-        pagerState = pagerState,
-        showAllTabs = showAllTabs
-    )
+        ExploreTabs(
+            pagerState = pagerState,
+            showAllTabs = showAllTabs
+        )
+    }
 }
 
