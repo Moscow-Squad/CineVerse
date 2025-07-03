cabd2d86da91da5a1803e6c64b8d3ba40cd9944e
alaafawzy <alaaf9443@gmail.com>
2025-07-03 18:48:33 +0300

    add animation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.compose.ui:ui:1.6.1")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.animation:animation:1.6.1")
+
+
+
 }
