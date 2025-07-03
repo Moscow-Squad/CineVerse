1310668dbcaa988bcd60581091da5f39368fda55
alaafawzy <alaaf9443@gmail.com>
2025-07-03 18:23:37 +0300

    improve: implement TextWithIcon

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/TextWithIcon.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/TextWithIcon.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.design_system.R
 import com.moscow.cineverse.designSystem.theme.Theme
 
 @Composable
@@ -36,4 +38,13 @@ fun TextWithIcon(
             style = Theme.textStyle.label.medium.regular,
         )
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun TextWithIconPreview() {
+    TextWithIcon(
+        icon = R.drawable.outline_location,
+        text = "location"
+    )
 }
