e1690b8ada5944483ab0270dc72e63260b19ea0c
Israa Mohamed <e.mohamed@shabek.com>
2025-07-04 02:46:42 +0300

    design cast card

---
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/movieSeriesDetails/CastMember.kt
@@ -0,0 +1,7 @@
+package com.moscow.cineverse.designSystem.component.movieSeriesDetails
+
+data class CastMember(
+    val realName: String,
+    val nameInMovie: String,
+    val imageUrl: String? = null
+)
