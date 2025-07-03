baa24740709007a37a7756df9c0063b848afcd46
alaafawzy <alaaf9443@gmail.com>
2025-07-03 21:33:45 +0300

    feat: resolve comment

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/TextWithIcon.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/TextWithIcon.kt
@@ -1,5 +1,6 @@
 package com.moscow.cineverse.designSystem.component
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -7,17 +8,21 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.design_system.R
+import com.moscow.cineverse.designSystem.theme.CineVerseTheme
 import com.moscow.cineverse.designSystem.theme.Theme
 
 @Composable
 
 fun TextWithIcon(
-    icon: Int,
     text: String,
+    @DrawableRes icon: Int,
+    textColour: Color,
+    iconTint: Color,
     modifier: Modifier = Modifier,
 ) {
 
@@ -26,7 +31,7 @@ fun TextWithIcon(
         Icon(
             painter = painterResource(icon),
             contentDescription = null,
-            tint = Theme.colors.shade.secondary,
+            tint = iconTint,
             modifier = Modifier
                 .size(14.33.dp)
                 .padding(end = 4.dp)
@@ -34,7 +39,7 @@ fun TextWithIcon(
         )
         Text(
             text = text,
-            color = Theme.colors.shade.secondary,
+            color = textColour,
             style = Theme.textStyle.label.medium.regular,
         )
     }
@@ -43,8 +48,12 @@ fun TextWithIcon(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun TextWithIconPreview() {
-    TextWithIcon(
-        icon = R.drawable.outline_location,
-        text = "location"
-    )
+    CineVerseTheme {
+        TextWithIcon(
+            icon = R.drawable.outline_location,
+            text = "location",
+            iconTint = Theme.colors.shade.secondary,
+            textColour = Theme.colors.shade.secondary
+        )
+    }
 }
