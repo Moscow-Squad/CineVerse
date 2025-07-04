b8830dd992c25cfc7ec78b85e76ee7e9d9fbec8b
Israa Mohamed <e.mohamed@shabek.com>
2025-07-04 05:38:08 +0300

    handle add to collection preview

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/movieSeriesDetails/AddToCollectionContent.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/movieSeriesDetails/AddToCollectionContent.kt
@@ -1,8 +1,12 @@
 package com.moscow.cineverse.designSystem.component.movieSeriesDetails
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -10,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.moscow.cineverse.designSystem.theme.CineVerseTheme
+import com.moscow.cineverse.designSystem.theme.Theme
 
 @Composable
 fun AddToCollectionContent(
@@ -41,10 +46,17 @@ fun AddToCollectionContent(
 @Composable
 private fun AddToCollectionContentPreview() {
     CineVerseTheme {
-        AddToCollectionContent(
-            collectionItems = listOf("Favorites", "Watchlist", "To Rewatch"),
-            selectedItem = "Favorites",
-            onItemSelected = {}
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Theme.colors.background.screen)
+                .padding(10.dp)
+        ) {
+            AddToCollectionContent(
+                collectionItems = listOf("Favorites", "Watchlist", "To Rewatch"),
+                selectedItem = "Favorites",
+                onItemSelected = {}
+            )
+        }
     }
 }
