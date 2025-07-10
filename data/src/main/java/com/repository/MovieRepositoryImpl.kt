dc4f0fce8a3110a52951e47c10e8405ee66d1954
Israa Mohamed <e.mohamed@shabek.com>
2025-07-10 16:46:57 +0300

    search setup

--- data/src/main/java/com/repository/MovieRepositoryImpl.kt
+++ data/src/main/java/com/repository/MovieRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package com.repository
 
-import com.android.domain.MovieRepository
+import com.android.domain.repository.MovieRepository
 import com.utils.BaseRepository
 
 class MovieRepositoryImpl :MovieRepository, BaseRepository() {
