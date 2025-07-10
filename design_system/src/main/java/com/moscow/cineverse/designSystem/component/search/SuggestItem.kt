f94fd98da23756bfd88a6a65735e36fc0e6b7780
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-10 17:30:07 +0300

    feat: add suggest item component

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/search/SuggestItem.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/search/SuggestItem.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.design_system.R
@@ -44,9 +45,18 @@ fun SuggestItem(
         )
         Icon(
             modifier = Modifier.size(20.dp),
-            painter = painterResource(Theme.icons.outline.arrowRightUp),
+            painter = painterResource(Theme.icons.outline.arrowLeftUp),
             contentDescription = stringResource(R.string.arrow_left_up),
             tint = Theme.colors.shade.secondary
         )
     }
+}
+
+@Preview
+@Composable
+private fun SuggestItemPreview() {
+    SuggestItem(
+        "dddddddddddd",
+        Theme.icons.outline.search
+    )
 }
