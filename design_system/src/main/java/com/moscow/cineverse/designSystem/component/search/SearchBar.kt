226072c16a27b5dc5d835be94b0881f1d1b27603
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-11 01:03:12 +0300

    refactor: make search bar have arrow back and cancel buttons

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/search/SearchBar.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/search/SearchBar.kt
@@ -1,36 +1,38 @@
 package com.moscow.cineverse.designSystem.component.search
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.platform.LocalFocusManager
 import com.example.design_system.R
 import com.moscow.cineverse.designSystem.theme.Theme
+import kotlinx.coroutines.delay
 
 @Composable
 fun SearchBar(
@@ -44,26 +46,56 @@ fun SearchBar(
     keyboardActions: KeyboardActions = KeyboardActions.Default,
     onCancelButtonClicked: () -> Unit = {}
 ) {
-    var hasFocusedOnce by remember { mutableStateOf(false) }
     var isFocused by remember { mutableStateOf(false) }
-
+    var blockRefocus by remember { mutableStateOf(false) }
     val focusRequester = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
 
-    Column(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+    LaunchedEffect(blockRefocus) {
+        if (blockRefocus) {
+            delay(100)
+            blockRefocus = false
+        }
+    }
+
+    LaunchedEffect(isFocused) {
+        if (!isFocused) {
+            focusManager.clearFocus(force = true)
+            focusRequester.freeFocus()
+        }
+    }
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(6.dp),
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
     ) {
+        if (isFocused || value.isNotEmpty()) {
+            Icon(
+                painter = painterResource(Theme.icons.outline.arrowLeft),
+                contentDescription = stringResource(R.string.search_icon),
+                tint = Theme.colors.shade.primary,
+                modifier = Modifier
+                    .size(20.dp)
+                    .clickable(onClick = {
+                        focusManager.clearFocus(force = true)
+                        focusRequester.freeFocus()
+                        isFocused = false
+                        blockRefocus = true
+                        onCancelButtonClicked()
+                    })
+            )
+        }
+
         OutlinedTextField(
             value = value,
             onValueChange = onValueChange,
             modifier = Modifier
-                .fillMaxWidth()
                 .focusRequester(focusRequester)
                 .onFocusChanged { focusState ->
-                    if (focusState.isFocused && !hasFocusedOnce) hasFocusedOnce = true
-
-                    isFocused = focusState.isFocused
+                    if (!blockRefocus) {
+                        isFocused = focusState.isFocused
+                    }
                 }
                 .background(Theme.colors.background.card, RoundedCornerShape(Theme.radius.large)),
             textStyle = Theme.textStyle.body.medium.medium,
@@ -87,25 +119,22 @@ fun SearchBar(
             },
             trailingIcon = {
                 if (isFocused || value.isNotEmpty()) {
-                    IconButton(
-                        onClick = {
-                            focusManager.clearFocus(force = true)
-                            onCancelButtonClicked()
-                            hasFocusedOnce = false
-                            isFocused = false
-                        }
-                    ) {
-                        Icon(
-                            painter = painterResource(Theme.icons.outline.xClose),
-                            contentDescription = stringResource(R.string.search_icon),
-                            tint = Theme.colors.shade.tertiary,
-                            modifier = Modifier.size(20.dp)
-                        )
-                    }
+                    Icon(
+                        painter = painterResource(Theme.icons.outline.xClose),
+                        contentDescription = stringResource(R.string.search_icon),
+                        tint = Theme.colors.shade.tertiary,
+                        modifier = Modifier
+                            .size(20.dp)
+                            .clickable(onClick = {
+                                focusManager.clearFocus(force = true)
+                                focusRequester.freeFocus()
+                                isFocused = false
+                                blockRefocus = true
+                                onCancelButtonClicked()
+                            })
+                    )
                 } else {
-                    IconButton(onClick = { }) {
-                        trailingIcon()
-                    }
+                    trailingIcon()
                 }
             },
             keyboardOptions = keyboardOptions,
@@ -118,7 +147,7 @@ fun SearchBar(
                 errorBorderColor = Theme.colors.additional.primary.red,
                 errorTextColor = Theme.colors.shade.primary,
                 cursorColor = Theme.colors.brand.primary,
-                errorCursorColor = Color.Transparent,
+                errorCursorColor = Color.Transparent
             )
         )
     }
