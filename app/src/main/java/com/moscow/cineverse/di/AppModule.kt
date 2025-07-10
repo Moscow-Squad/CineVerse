2328980a3c2b92e5790265b2ef9f5bb7f2df5c63
kareem esam <kareem.eldeen.esam@gmail.com>
2025-07-10 23:16:27 +0200

    Integrate WorkManager for search history deletion and set up repository module
    # Conflicts:
    #	app/src/main/java/com/moscow/cineverse/di/AppModule.kt
    #	data/src/main/java/com/di/RepositoryModule.kt
    #	data/src/main/java/com/repository/SearchRepositoryImpl.kt

--- app/src/main/java/com/moscow/cineverse/di/AppModule.kt
+++ app/src/main/java/com/moscow/cineverse/di/AppModule.kt
@@ -2,5 +2,6 @@ package com.moscow.cineverse.di
 
 import com.di.dataModule
 import com.di.repositoryModule
+import com.di.workManagerModule
 
-val appModule = dataModule + presentationModule  + repositoryModule
+val appModule = dataModule + presentationModule + workManagerModule + repositoryModule
--- data/src/main/java/com/repository/search/SearchRepositoryImpl.kt
+++ data/src/main/java/com/repository/search/SearchRepositoryImpl.kt
@@ -1,14 +1,20 @@
 package com.repository.search
 
 import androidx.room.Transaction
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.android.domain.SearchRepository
 import com.android.domain.model.Movie
+import com.remote.DeleteQueryWorker
 import com.repository.mapper.toDomain
 import com.repository.mapper.toEntity
 import com.utils.BaseRepository
+import java.util.concurrent.TimeUnit
 
 class SearchRepositoryImpl(
     private val searchLocalDateSource: SearchLocalDateSource,
+    private val workManager: WorkManager
 ) : SearchRepository, BaseRepository(
 ) {
     override suspend fun getMoviesBySearchTerm(searchTerm: String): List<Movie> {
@@ -19,6 +25,13 @@ class SearchRepositoryImpl(
     override suspend fun insertMovie(movies: List<Movie>, searchTerm: String) {
         searchLocalDateSource.insertSearchHistory(searchTerm)
         searchLocalDateSource.insertMovie(movies.toEntity(searchTerm), searchTerm)
+        val deleteWork = OneTimeWorkRequestBuilder<DeleteQueryWorker>()
+            .setInitialDelay(1, TimeUnit.MINUTES)
+            .setInputData(workDataOf("query" to searchTerm))
+            .addTag("delete_search_query_history")
+            .build()
+
+        workManager.enqueue(deleteWork)
     }
     override suspend fun getSearchHistory(): List<String> {
         return searchLocalDateSource.getAllSearchHistory()
