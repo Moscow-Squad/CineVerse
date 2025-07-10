16136aef3f3922b50d97de7b1874fbaa479f333d
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-10 13:58:55 +0300

    feat: add implementation for get local suggestions use case

--- domain/src/main/java/com/android/domain/usecase/GetLocalSuggestions.kt
+++ domain/src/main/java/com/android/domain/usecase/GetLocalSuggestions.kt
@@ -1,9 +1,11 @@
 package com.android.domain.usecase
 
+import com.android.domain.exception.CineVerseException.NoSuggestionFoundException
 import com.android.domain.repository.MovieRepository
 
-class GetLocalSuggestions( val movieRepository: MovieRepository ) {
-    operator fun invoke() : List<String> {
-        return emptyList()
+class GetLocalSuggestions(private val movieRepository: MovieRepository) {
+    operator fun invoke(): List<String> {
+        val suggestions = movieRepository.getLocalSuggestions()
+        return if (suggestions.isEmpty()) throw NoSuggestionFoundException() else suggestions
     }
 }
