1cdbc6dd81e8aa74746801b21b77c96429b228a9
alaafawzy <alaaf9443@gmail.com>
2025-07-12 09:13:55 +0300

    Add ActorEntity and SeriesEntity handling to SearchLocalDateSource

--- data/src/main/java/com/repository/search/SearchLocalDateSource.kt
+++ data/src/main/java/com/repository/search/SearchLocalDateSource.kt
@@ -1,6 +1,8 @@
 package com.repository.search
 
+import com.local.entity.ActorEntity
 import com.local.entity.MovieEntity
+import com.local.entity.SeriesEntity
 
 interface SearchLocalDateSource {
     suspend fun getAllSearchHistory(): List<String>
@@ -12,5 +14,11 @@ interface SearchLocalDateSource {
     suspend fun insertMovie(moviesEntity: List<MovieEntity>, searchTerm: String)
 
     suspend fun getMoviesBySearchTerm(searchTerm: String): List<MovieEntity>
+
+    suspend fun insertActors(actors: List<ActorEntity>, searchTerm: String)
+    suspend fun getActorsBySearchTerm(searchTerm: String): List<ActorEntity>
+
+    suspend fun insertSeries(series: List<SeriesEntity>, searchTerm: String)
+    suspend fun getSeriesBySearchTerm(searchTerm: String): List<SeriesEntity>
 
 }
