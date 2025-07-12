3a5b5a7650d93645ce8d397132ceb6a443d7aee2
alaafawzy <alaaf9443@gmail.com>
2025-07-12 09:09:45 +0300

    feat: create SeriesDao

---
+++ data/src/main/java/com/local/dao/search/SeriesDao.kt
@@ -0,0 +1,16 @@
+package com.local.dao.search
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.local.entity.SeriesEntity
+
+@Dao
+interface SeriesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSeries(series: List<SeriesEntity>)
+
+    @Query("SELECT * FROM series_table WHERE searchTerm = :searchTerm")
+    suspend fun getSeriesBySearchTerm(searchTerm: String): List<SeriesEntity>
+}
