5ea4ac8a66f88b3af7d1fefd38cf7d6809988479
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-07-14 22:08:19 +0300

    feature/finish review card and rating bar component

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/movieSeriesDetails/RatingBar.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/movieSeriesDetails/RatingBar.kt
@@ -1,21 +1,23 @@
 package com.moscow.cineverse.designSystem.component.movieSeriesDetails
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import com.example.design_system.R
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.moscow.cineverse.designSystem.theme.CineVerseTheme
+import com.moscow.cineverse.designSystem.theme.Theme
 
 @Composable
 fun MovieRatingBar(
@@ -28,16 +30,25 @@ fun MovieRatingBar(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
+
         for (i in 1..5) {
-            IconButton(modifier = Modifier.size(16.dp),
-                onClick = { onRatingChanged(i.toFloat()) }) {
-                Icon(
+            val isFilled = i <= rating
+            val targetColor = if (isFilled) Theme.colors.additional.primary.yellow else Theme.colors.shade.tertiary
+
+            val animatedTint by animateColorAsState(
+                targetValue = targetColor,
+                animationSpec = tween(durationMillis = 300),
+                label = "star_tint_animation"
+            )
+            Icon(
                     if (i <= rating) painterResource(R.drawable.yellow_star) else painterResource(R.drawable.outline_star),
                     contentDescription = stringResource(R.string.star_icon),
-                    modifier = Modifier.fillMaxSize(),
-                    tint = Color.Unspecified
-                )
-            }
+                    modifier =
+                        Modifier
+                        .size(16.dp)
+                        .clickable{ onRatingChanged(i.toFloat()) },
+                    tint = animatedTint
+            )
         }
     }
 
