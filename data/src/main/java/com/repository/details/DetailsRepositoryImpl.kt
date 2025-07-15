13d674d068190dae92801a0d939beb3490744ef6
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-07-15 03:38:01 +0300

    add mapper, repository, and use cases for review

--- data/src/main/java/com/repository/details/DetailsRepositoryImpl.kt
+++ data/src/main/java/com/repository/details/DetailsRepositoryImpl.kt
@@ -2,10 +2,12 @@ package com.repository.details
 
 
 import com.android.domain.model.MovieDetail
+import com.android.domain.model.Review
 import com.android.domain.model.SeriesDetail
 import com.android.domain.repository.DetailsRepository
 import com.mapper.toDomain
 import com.remote.source.DetailsRemoteDataSource
+import com.repository.mapper.toDomain
 
 class DetailsRepositoryImpl(
     private val detailsRemoteDataSource: DetailsRemoteDataSource,
@@ -15,4 +17,10 @@ class DetailsRepositoryImpl(
 
     override suspend fun getSeriesDetail(seriesId: Int): SeriesDetail =
         detailsRemoteDataSource.getSeriesDetails(seriesId).toDomain()
+
+
+    override suspend fun getReviewsPage(id:Int, page: Int, isMovie: Boolean): List<Review> {
+        val response = detailsRemoteDataSource.getReviews(id, page, isMovie)
+        return response.results.orEmpty().mapNotNull { it?.toDomain() }
+    }
 }
