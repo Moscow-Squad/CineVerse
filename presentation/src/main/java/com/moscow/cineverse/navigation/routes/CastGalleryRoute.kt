ed42399f9aaa561602d52efbbf578d92d252598e
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-15 16:00:22 +0300

    feat: add navigation for cast gallery and cast best of movie

--- presentation/src/main/java/com/moscow/cineverse/navigation/routes/CastGalleryRoute.kt
+++ presentation/src/main/java/com/moscow/cineverse/navigation/routes/CastGalleryRoute.kt
@@ -1,17 +1,22 @@
 package com.moscow.cineverse.navigation.routes
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
+import com.moscow.cineverse.screen.castDetails.gallery.ActorGalleryScreen
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CastGalleryRoute(val castId: Int)
+data class CastGalleryRoute(val castId: Int, val castName: String)
 
 fun NavGraphBuilder.CastGalleryRoute(navController: NavHostController) {
     composable<CastGalleryRoute>{
         val args = it.toRoute<CastGalleryRoute>()
+        ActorGalleryScreen(
+            navController = navController,
+            actorId = args.castId,
+            title = args.castName,
+        )
     }
 }
