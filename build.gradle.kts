4ac0fd318795c5ca2bbf3b1554be7113a9a49f1f
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-07-22 15:20:17 +0300

    add kover to build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,44 @@ plugins {
     alias(libs.plugins.google.firebase.firebase.perf) apply false
     alias(libs.plugins.google.firebase.appdistribution) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kover)
+}
+
+val excludedPackages = listOf(
+    "*.R",
+    "*.R_*",
+    "**.di.**",
+    "**.logging.**",
+    "*.BuildConfig*",
+    "*.Manifest*",
+    "*.ComposableSingletons*",
+    "*.MainActivity*",
+    "*.CineVerseApp*",
+    "com.example.image_viewer.*",
+    "com.example.design_system.*",
+    "entity.**",
+    "**.dao.**",
+    "**.dto.**",
+    "**.response.**",
+    "exceptions.**",
+)
+allprojects {
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+    kover {
+        reports {
+            filters {
+                excludes { classes(excludedPackages) }
+            }
+            total {
+                verify {
+                    rule { bound { minValue = 80 } }
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+
 }
