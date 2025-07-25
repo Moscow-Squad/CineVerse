b2dc34a52302d607a25f7a16a55ff5c0b7aafe61
khaledeid1k <khaled.eid1k1k@gmail.com>
2025-07-25 16:06:59 +0200

    Fix: Correct NavHost startDestination in CineVerseNavGraph
    The `startDestination` for the `NavHost` in `CineVerseNavGraph.kt` was hardcoded to `ExploreRoute`.
    
    This commit changes it to use the `startDestination` parameter passed to the `CineVerseNavGraph` composable function, allowing for dynamic initial routing.

--- presentation/src/main/java/com/moscow/cineverse/navigation/CineVerseNavGraph.kt
+++ presentation/src/main/java/com/moscow/cineverse/navigation/CineVerseNavGraph.kt
@@ -9,7 +9,6 @@ import androidx.navigation.compose.NavHost
 import com.moscow.cineverse.navigation.routes.CastBestOfMovieRoute
 import com.moscow.cineverse.navigation.routes.CastDetailsRoute
 import com.moscow.cineverse.navigation.routes.CastGalleryRoute
-import com.moscow.cineverse.navigation.routes.ExploreRoute
 import com.moscow.cineverse.navigation.routes.MovieDetailsRoute
 import com.moscow.cineverse.navigation.routes.RecommendationsRoute
 import com.moscow.cineverse.navigation.routes.ReviewsRoute
@@ -37,7 +36,7 @@ fun CineVerseNavGraph(
     CompositionLocalProvider(
         LocalNavController provides navController
     ) {
-        NavHost(modifier = modifier, navController = navController, startDestination = ExploreRoute) {
+        NavHost(modifier = modifier, navController = navController, startDestination = startDestination) {
             exploreRoute()
             loginRoute()
             RecommendationsRoute()
