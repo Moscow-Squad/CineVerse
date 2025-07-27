a909675b0c5e4fd1bee0585202e4db24b56b2803
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-27 21:32:37 +0300

    refactor: navigate to movie details

--- presentation/src/main/java/com/moscow/cineverse/screen/movie_details/recommendations/RecommendationsMoviesViewModel.kt
+++ presentation/src/main/java/com/moscow/cineverse/screen/movie_details/recommendations/RecommendationsMoviesViewModel.kt
@@ -36,7 +36,7 @@ class RecommendationsMoviesViewModel (
     }
 
     override fun onMovieClick(movieId: Int) {
-
+        sendEvent(RecommendationMoviesEffect.NavigateToMovieDetailsBack(movieId))
     }
 
     override fun backButtonClick() {
