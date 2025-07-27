d97b776e9fb5e15d9718957ddcdbf138472409d8
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-27 18:22:43 +0300

    refactor: image viewer module gradle

--- image_viewer/build.gradle.kts
+++ image_viewer/build.gradle.kts
@@ -9,18 +9,9 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.ui.tooling.preview.android)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 
     api(libs.coil3.compose)
     implementation(libs.cloudy)
 
-    implementation(libs.tensorflow.lite.vision)
     implementation(libs.tensorflow.lite)
-    implementation(libs.tensorflow.lite.support)
-
 }
