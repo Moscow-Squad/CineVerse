c9d4c1881914aa90df092c2fde174d2381ae27fd
Hazem Alkateb <hazem22-1@hotmail.com>
2025-07-30 14:56:35 +0300

    fix bottom sheet padding

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/bottomsheet/CineVerseBottomSheet.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/bottomsheet/CineVerseBottomSheet.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -70,7 +71,7 @@ fun CineVerseBottomSheet(
         shape = RoundedCornerShape(Theme.radius.extraLarge),
         modifier = Modifier
             .padding(horizontal = 12.dp)
-            .padding(bottom = 28.dp),
+            .navigationBarsPadding(),
         dragHandle = {
             Box(
                 modifier = Modifier
