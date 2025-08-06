b07ec1cca74e6af74536f50ffc3a865616ec6d04
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-08-06 15:48:46 +0300

    refactor: integrate language selection into MainActivity

--- app/src/main/java/com/moscow/cineverse/main_activity/MainActivityUiState.kt
+++ app/src/main/java/com/moscow/cineverse/main_activity/MainActivityUiState.kt
@@ -1,7 +1,7 @@
-package com.moscow.cineverse
-
+package com.moscow.cineverse.main_activity
 
 data class MainActivityUiState(
     val isLoading: Boolean = false,
     val isDarkTheme: Boolean = false,
+    val language: String = "en"
 )
--- app/src/main/java/com/moscow/cineverse/main_activity/MainActivityViewModel.kt
+++ app/src/main/java/com/moscow/cineverse/main_activity/MainActivityViewModel.kt
@@ -1,22 +1,22 @@
-package com.moscow.cineverse
+package com.moscow.cineverse.main_activity
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.moscow.domain.repository.language.LanguageProvider
 import com.moscow.domain.repository.theme.ThemeProvider
-import kotlinx.coroutines.Dispatchers
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import androidx.lifecycle.viewModelScope
-import com.moscow.domain.repository.language.LanguageProvider
-import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     private val themeProvider: ThemeProvider,
+    private val languageProvider: LanguageProvider
+) : ViewModel() {
 
-): ViewModel() {
 
     private val _state = MutableStateFlow(MainActivityUiState())
     val state = _state.asStateFlow()
@@ -27,16 +27,19 @@ class MainActivityViewModel @Inject constructor(
 
     private fun loadScreen() {
         _state.update { it.copy(isLoading = true) }
+
         viewModelScope.launch {
             themeProvider.themeFlow.collect { isDarkTheme ->
+                _state.update { it.copy(isDarkTheme = isDarkTheme) }
+            }
+        }
+
+        viewModelScope.launch {
+            languageProvider.languageFlow.collect { lang ->
                 _state.update {
-                    it.copy(
-                        isDarkTheme = isDarkTheme,
-                        isLoading = false
-                    )
+                    it.copy(language = lang, isLoading = false)
                 }
             }
-
         }
     }
 
