865a9d79a116b218926aa5802e7111a36f808bc9
Hazem Alkateb <hazem22-1@hotmail.com>
2025-08-06 15:32:53 +0300

    implement on boarding screen ui - handle switching between images. - handle show/hide buttons for swiping. - write implementation of buttons in viewmodel. - handle the animation of pager and text. - add texts in string files to support arabic language. - make the arrow icons support auto mirrored.

---
+++ presentation/src/main/java/com/moscow/cineverse/screen/on_boarding/OnBoardingViewModel.kt
@@ -0,0 +1,61 @@
+package com.moscow.cineverse.screen.on_boarding
+
+import com.moscow.cineverse.base.BaseViewModel
+import com.moscow.cineverse.utlis.StringValue
+import com.moscow.cinverse.presentation.R
+
+class OnBoardingViewModel :
+    BaseViewModel<OnBoardingState, OnBoardingScreenEvents>(OnBoardingState()),
+    OnBoardingInteractionListener {
+
+    init {
+        getPages()
+    }
+
+    private fun getPages() {
+        updateState {
+            it.copy(
+                pages =
+                    listOf(
+                        PageUiState(
+                            imageResId = R.drawable.on_boarding_1,
+                            title = StringValue.StringResource(R.string.welcome_to_your_movie_universe),
+                            description = StringValue.StringResource(R.string.discover_track_and_rate_your_favorite_movies_series)
+                        ),
+                        PageUiState(
+                            imageResId = R.drawable.on_boarding_2,
+                            title = StringValue.StringResource(R.string.track_everything),
+                            description = StringValue.StringResource(R.string.your_watchlist_your_ratings_all_in_one_place)
+                        ),
+                        PageUiState(
+                            imageResId = R.drawable.on_boarding_3,
+                            title = StringValue.StringResource(R.string.personalized_recommendations),
+                            description = StringValue.StringResource(R.string.answer_fun_questions_to_get_handpicked_recommendations)
+                        )
+                    )
+            )
+        }
+    }
+
+    override fun onPageChanged(pageIndex: Int) {
+        updateState { it.copy(currentPage = pageIndex) }
+    }
+
+    override fun onClickPreviousButton() {
+        val prev = uiState.value.currentPage - 1
+        if (prev >= 0) {
+            updateState { it.copy(currentPage = prev) }
+        }
+    }
+
+    override fun onClickNextButton() {
+        val next = uiState.value.currentPage + 1
+        if (next < uiState.value.pages.size) {
+            updateState { it.copy(currentPage = next) }
+        }
+    }
+
+    override fun onClickGetStartedButton() {
+        // go to login screen
+    }
+}
