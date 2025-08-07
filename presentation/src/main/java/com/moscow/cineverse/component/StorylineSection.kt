e242ff897e770ddcd190fc5099bcfb5bf1638363
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-08-07 17:36:57 +0300

    refactor: remove unused import

--- presentation/src/main/java/com/moscow/cineverse/component/StorylineSection.kt
+++ presentation/src/main/java/com/moscow/cineverse/component/StorylineSection.kt
@@ -5,15 +5,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.moscow.cineverse.designSystem.theme.Theme
 import com.moscow.cinverse.presentation.R
 
@@ -23,8 +17,6 @@ fun StorylineSection(
     description: String?,
     modifier: Modifier = Modifier
 ) {
-    val textColor = Theme.colors.shade.secondary
-
     if (!description.isNullOrEmpty()) {
         Text(
             text = stringResource(R.string.storyline),
@@ -32,7 +24,6 @@ fun StorylineSection(
             color = Theme.colors.shade.primary,
             modifier = modifier.padding(16.dp, top = 24.dp, bottom = 8.dp),
         )
-
         Text(
             modifier = Modifier.padding(horizontal = 16.dp),
             overflow = TextOverflow.Ellipsis,
@@ -42,5 +33,4 @@ fun StorylineSection(
             textAlign = TextAlign.Justify
         )
     }
-
 }
