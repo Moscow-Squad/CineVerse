294e82e1730d295b121de07754f7b69322c609b6
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-08-08 04:58:24 +0300

    refactor: enhance SplashScreen with RTL support and code formatting improvements

--- presentation/src/main/java/com/moscow/cineverse/screen/splash/SplashScreen.kt
+++ presentation/src/main/java/com/moscow/cineverse/screen/splash/SplashScreen.kt
@@ -8,9 +8,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.moscow.cineverse.designSystem.theme.CineVerseTheme
 import com.moscow.cineverse.designSystem.theme.Theme
@@ -19,27 +22,29 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
-    navigateToHome:()->Unit,
-    navigateToLogin:()->Unit,
-    navigateToOnboarding:()->Unit,
+    navigateToHome: () -> Unit,
+    navigateToLogin: () -> Unit,
+    navigateToOnboarding: () -> Unit,
     modifier: Modifier = Modifier,
     splashViewModel: SplashViewModel = hiltViewModel(),
-    ) {
+) {
 
 
     LaunchedEffect(splashViewModel) {
 
         delay(3000)
         splashViewModel.getDestination()
-        splashViewModel.uiEffect.collect {effect ->
-            when(effect){
+        splashViewModel.uiEffect.collect { effect ->
+            when (effect) {
                 SplashEvent.NavigateToHome -> {
                     navigateToHome()
                 }
+
                 SplashEvent.NavigateToLogin -> {
 
                     navigateToLogin()
                 }
+
                 SplashEvent.NavigateToOnboarding -> {
 
                     navigateToOnboarding()
@@ -58,7 +63,10 @@ fun SplashContent(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
             .fillMaxSize()
-            .background(Theme.colors.background.screen),
+            .background(Theme.colors.background.screen)
+            .then(if (LocalLayoutDirection.current == LayoutDirection.Rtl) Modifier.graphicsLayer(
+                scaleX = -1f
+            ) else Modifier),
         contentAlignment = Alignment.Center
 
     ) {
@@ -68,7 +76,7 @@ fun SplashContent(modifier: Modifier = Modifier) {
             contentDescription = "splash image",
             contentScale = ContentScale.Crop,
 
-        )
+            )
     }
 
 }
@@ -83,5 +91,5 @@ private fun SplashPreview() {
             {}
         )
     }
-    
+
 }
