2fa70fe34e8a2fffdf2148643d55e3eb3c4c89b3
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-08-10 01:18:34 +0300

    add textPaddings parameter to MovieAppBar

--- design_system/src/main/java/com/moscow/cineverse/designSystem/component/MovieAppBar.kt
+++ design_system/src/main/java/com/moscow/cineverse/designSystem/component/MovieAppBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -28,6 +29,7 @@ fun MovieAppBar(
     modifier: Modifier = Modifier,
     backButtonClick: () -> Unit = {},
     addButtonClick: () -> Unit = {},
+    textPaddings: PaddingValues = PaddingValues(0.dp),
     title: String? = null,
     caption: String? = null,
     showBackButton: Boolean = true,
@@ -68,7 +70,7 @@ fun MovieAppBar(
                 )
             }
             Column(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier.weight(1f).padding(textPaddings),
             ) {
                 if (caption != null) {
                     Text(
@@ -85,7 +87,7 @@ fun MovieAppBar(
                         style = Theme.textStyle.title.small,
                         color = Theme.colors.shade.primary,
                         maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
+                        overflow = TextOverflow.Ellipsis,
                     )
                 }
             }
