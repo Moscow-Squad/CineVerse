8ca55e7012688b582aa4c225110eec3fa6dd339f
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-08-11 14:26:40 +0300

    Refactor: move MediaType enum to MediaItem class

--- data/src/main/java/com/moscow/remote/dto/MediaItemDto.kt
+++ data/src/main/java/com/moscow/remote/dto/MediaItemDto.kt
@@ -5,11 +5,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MediaItemDto(
+    @SerialName("id") val id: Int,
     @SerialName("adult") val adult: Boolean? = null,
     @SerialName("backdrop_path") val backdropPath: String? = null,
     @SerialName("first_air_date") val firstAirDate: String? = null,
     @SerialName("genre_ids") val genreIds: List<Int>? = null,
-    @SerialName("id") val id: Int? = null,
     @SerialName("media_type") val mediaType: String? = null,
     @SerialName("name") val name: String? = null,
     @SerialName("origin_country") val originCountry: List<String>? = null,
--- domain/src/main/java/com/moscow/domain/model/MediaItem.kt
+++ domain/src/main/java/com/moscow/domain/model/MediaItem.kt
@@ -1,11 +1,11 @@
 package com.moscow.domain.model
 
 data class MediaItem(
+    val id: Int,
     val adult: Boolean,
     val backdropPath: String,
     val firstAirDate: String,
     val genreIds: List<Int>,
-    val id: Int,
     val mediaType: MediaType,
     val name: String,
     val originalName: String,
@@ -13,4 +13,20 @@ data class MediaItem(
     val popularity: Double,
     val posterPath: String,
     val rate: Float,
-)
+){
+    enum class MediaType {
+        Movie,
+        Tv,
+        Person,
+        Unknown;
+
+        override fun toString(): String {
+            return name.lowercase()
+        }
+        companion object {
+            fun toMediaType(value: String): MediaType {
+                return entries.find { it.name.equals(value, ignoreCase = true) } ?: Unknown
+            }
+        }
+    }
+}
