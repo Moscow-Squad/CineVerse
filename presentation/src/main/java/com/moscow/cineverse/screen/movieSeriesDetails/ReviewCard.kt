377e16879360f46167efafb46f375d81fbdc5872
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-08-13 17:08:06 +0300

    feat: implement pagination and error handling in reviews screen

--- presentation/src/main/java/com/moscow/cineverse/screen/movieSeriesDetails/ReviewCard.kt
+++ presentation/src/main/java/com/moscow/cineverse/screen/movieSeriesDetails/ReviewCard.kt
@@ -36,7 +36,7 @@ fun MovieReviewCard(
     name: String,
     username: String,
     reviewText: String,
-    rating: Int,
+    rating: Float,
     date: String,
     avatar: Painter?,
     modifier: Modifier = Modifier
@@ -65,11 +65,12 @@ fun MovieReviewCard(
 
         Row(Modifier.fillMaxWidth()) {
 
-            if (rating != 0)
+            if (rating != 0.0f)
                 MovieRatingBar(rating, {})
 
             Spacer(Modifier.weight(1f))
 
+            if(date.isNotBlank())
             Text(
                 text = date,
                 style = Theme.textStyle.body.small.regular,
@@ -159,7 +160,7 @@ private fun PreviewMovieReviewCard() {
             "Shrouk Mohamed",
             "ShroukMohamed16",
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            3,
+            3.0f,
             "Aug 15, 2025",
             painterResource(R.drawable.outline_user)
         )
