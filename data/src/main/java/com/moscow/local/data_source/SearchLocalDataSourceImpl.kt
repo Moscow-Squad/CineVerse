a112b4dfc1c4d2cfa46c4d99027d2e5d48e798ed
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-08-15 23:23:00 +0300

    refactor: get back delete work manager for search query

--- data/src/main/java/com/moscow/local/data_source/SearchLocalDataSourceImpl.kt
+++ data/src/main/java/com/moscow/local/data_source/SearchLocalDataSourceImpl.kt
@@ -1,16 +1,24 @@
 package com.moscow.local.data_source
 
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.moscow.data_source.local.SearchLocalDataSource
+import com.moscow.local.DeleteHistoryQueryWorker
 import com.moscow.local.dao.search.FavouriteGenreDao
 import com.moscow.local.dao.search.SearchHistoryDao
 import com.moscow.local.entity.FavouriteGenreEntity
 import com.moscow.local.entity.SearchHistoryEntity
+import com.moscow.utils.DELETE_SEARCH_QUERY_HISTORY
+import com.moscow.utils.QUERY
 import kotlinx.coroutines.flow.Flow
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class SearchLocalDataSourceImpl @Inject constructor(
     private val searchHistoryDao: SearchHistoryDao,
-    private val favouriteGenreDao: FavouriteGenreDao
+    private val favouriteGenreDao: FavouriteGenreDao,
+    private val workManager: WorkManager
 ) : SearchLocalDataSource {
 
     override suspend fun getAllSearchHistory(): Flow<List<String>> =
@@ -18,9 +26,17 @@ class SearchLocalDataSourceImpl @Inject constructor(
 
     override suspend fun insertSearchHistory(
         searchTerm: String
-    ) = searchHistoryDao.insertSearchHistory(
-        SearchHistoryEntity(searchTerm = searchTerm)
-    )
+    ) {
+        searchHistoryDao.insertSearchHistory(
+            SearchHistoryEntity(searchTerm = searchTerm)
+        )
+        val deleteWork = OneTimeWorkRequestBuilder<DeleteHistoryQueryWorker>()
+            .setInitialDelay(1, TimeUnit.MINUTES)
+            .setInputData(workDataOf(QUERY to searchTerm))
+            .addTag(DELETE_SEARCH_QUERY_HISTORY)
+            .build()
+        workManager.enqueue(deleteWork)
+    }
 
     override suspend fun deleteSearchHistory(
         searchTerm: String
