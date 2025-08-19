be6c78a5dd42a17704e4940bad3ac1852f776ded
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-08-19 13:42:44 +0300

    refactor: add animation to search bar

--- presentation/src/main/java/com/moscow/cineverse/screen/explore/component/SearchBar.kt
+++ presentation/src/main/java/com/moscow/cineverse/screen/explore/component/SearchBar.kt
@@ -8,17 +8,19 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,12 +28,14 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -42,6 +46,7 @@ import com.moscow.cineverse.designSystem.theme.Theme
 import com.moscow.cineverse.utlis.noRibbleClick
 import com.moscow.cinverse.presentation.R
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun SearchBar(
@@ -61,9 +66,10 @@ fun SearchBar(
     var blockRefocus by remember { mutableStateOf(false) }
     val focusRequester = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
+    val coroutineScope = rememberCoroutineScope()
 
     val paddingStart by animateDpAsState(
-        targetValue = if (isFocused || value.isNotEmpty()) 10.dp else 0.dp,
+        targetValue = if (isFocused || value.isNotEmpty()) 46.dp else 0.dp,
         animationSpec = tween(300)
     )
 
@@ -81,21 +87,23 @@ fun SearchBar(
         }
     }
 
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(6.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.fillMaxWidth()
+    Box(
+        modifier = modifier.fillMaxWidth(),
     ) {
         AnimatedVisibility(
             visible = isFocused || value.isNotEmpty(),
             enter = slideInHorizontally(initialOffsetX = { -it }) + fadeIn(animationSpec = tween(300)),
-            exit = slideOutHorizontally(targetOffsetX = { -it }) + fadeOut(animationSpec = tween(300))
+            exit = slideOutHorizontally(targetOffsetX = { -it }) + fadeOut(animationSpec = tween(300)),
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 10.dp)
         ) {
             Icon(
                 painter = painterResource(R.drawable.outline_arrow_left),
                 contentDescription = stringResource(R.string.search_icon),
                 tint = Theme.colors.shade.primary,
                 modifier = Modifier
+                    .align(Alignment.CenterStart)
                     .size(20.dp)
                     .noRibbleClick(
                         onClick = {
@@ -103,17 +111,21 @@ fun SearchBar(
                             focusRequester.freeFocus()
                             isFocused = false
                             blockRefocus = true
-                            onCancelButtonClicked()
-                        },
+                            coroutineScope.launch {
+                                delay(300)
+                                onCancelButtonClicked()
+                            }
+                         },
                     )
             )
         }
-        OutlinedTextField(
-            value = value,
-            onValueChange = onValueChange,
+
+        Box(
             modifier = Modifier
+                .align(Alignment.CenterStart)
                 .padding(start = paddingStart)
-                .weight(1f)
+                .fillMaxWidth()
+                .heightIn(min = 48.dp)
                 .focusRequester(focusRequester)
                 .onFocusChanged { focusState ->
                     if (!blockRefocus) {
@@ -123,27 +135,53 @@ fun SearchBar(
                         isFocused = focusState.isFocused
                     }
                 }
-                .background(Theme.colors.background.card, RoundedCornerShape(Theme.radius.large)),
-            textStyle = Theme.textStyle.body.medium.medium.copy(color = Theme.colors.shade.primary),
-            placeholder = {
-                Text(
-                    text = stringResource(com.moscow.cinverse.presentation.R.string.search),
-                    style = Theme.textStyle.body.medium.regular,
-                    color = Theme.colors.shade.tertiary,
-                    fontSize = 14.sp
-                )
-            },
-            singleLine = singleLine,
-            maxLines = maxLines,
-            leadingIcon = {
+                .background(Theme.colors.background.card, RoundedCornerShape(Theme.radius.large))
+                .border(
+                    width = 1.dp,
+                    color = Theme.colors.stroke.primary,
+                    shape = RoundedCornerShape(Theme.radius.large)
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 14.dp)
+            ) {
                 Icon(
                     painter = painterResource(R.drawable.outline_search),
                     contentDescription = stringResource(R.string.search_icon),
                     tint = Theme.colors.shade.tertiary,
                     modifier = Modifier.size(20.dp)
                 )
-            },
-            trailingIcon = {
+
+                Spacer(modifier = Modifier.size(8.dp))
+
+                // Text field
+                BasicTextField(
+                    value = value,
+                    onValueChange = onValueChange,
+                    textStyle = Theme.textStyle.body.medium.medium.copy(
+                        color = Theme.colors.shade.primary,
+                        fontSize = 14.sp
+                    ),
+                    singleLine = singleLine,
+                    maxLines = maxLines,
+                    keyboardOptions = keyboardOptions,
+                    keyboardActions = keyboardActions,
+                    cursorBrush = SolidColor(Theme.colors.brand.primary),
+                    modifier = Modifier.weight(1f)
+                ) { innerTextField ->
+                    if (value.isEmpty()) {
+                        Text(
+                            text = stringResource(R.string.search),
+                            style = Theme.textStyle.body.medium.regular,
+                            color = Theme.colors.shade.tertiary,
+                            fontSize = 14.sp
+                        )
+                    }
+                    innerTextField()
+                }
+
                 if (isFocused && value.isNotEmpty()) {
                     Icon(
                         painter = painterResource(R.drawable.outline_x),
@@ -158,26 +196,8 @@ fun SearchBar(
                 } else {
                     trailingIcon()
                 }
-            },
-            keyboardOptions = keyboardOptions,
-            keyboardActions = KeyboardActions (
-                onSearch = {
-                    keyboardActions.onSearch?.let { it() }
-                    isFocused = false
-                },
-                onDone = {
-                    keyboardActions.onDone?.let { it() }
-                    isFocused = false
-                }
-            ),
-            shape = RoundedCornerShape(Theme.radius.large),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedTextColor = Theme.colors.shade.primary,
-                focusedBorderColor = Theme.colors.stroke.primary,
-                unfocusedBorderColor = Theme.colors.stroke.primary,
-                cursorColor = Theme.colors.brand.primary,
-            ),
-        )
+            }
+        }
     }
 }
 
