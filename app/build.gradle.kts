899273f5f6170ff59015f215d1ddd3cda85332a3
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-08-20 21:05:30 +0300

    feat: implement splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,5 +96,8 @@ dependencies {
     implementation(libs.androidx.work.runtime.ktx)
 
     implementation(libs.androidx.navigation.compose)
+    // splash api
+    implementation (libs.androidx.core.splashscreen)
+
 
 }
--- presentation/src/main/java/com/moscow/cineverse/screen/details/common/RatingSection.kt
+++ presentation/src/main/java/com/moscow/cineverse/screen/details/common/RatingSection.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -50,6 +51,7 @@ fun RatingSection(
         )
         Row(
             modifier = Modifier
+                .fillMaxWidth()
                 .clip(RoundedCornerShape(Theme.radius.large))
                 .background(Theme.colors.background.card)
                 .clickable { onClick() },
