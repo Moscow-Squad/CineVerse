5d6999c1ddde03580032ab37e56dfc2cfe011b68
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-08-20 12:45:31 +0300

    refactor: make icon in home header centered horizontally

--- presentation/src/main/java/com/moscow/cineverse/screen/home/components/HomeHeader.kt
+++ presentation/src/main/java/com/moscow/cineverse/screen/home/components/HomeHeader.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -20,12 +22,13 @@ fun HomeHeader(userName: String?, modifier: Modifier = Modifier) {
     Row(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
 
         Image(
             painter = painterResource(R.drawable.colored_cineverse_logo),
             contentDescription = null,
-            modifier = Modifier.padding(start = 16.dp)
+            modifier = Modifier.padding(start = 16.dp).size(32.dp)
         )
 
         Column(
