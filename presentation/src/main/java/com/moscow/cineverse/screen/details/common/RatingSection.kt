41ceee563b11f27cfab3787a48ac86626fc4016f
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-08-21 17:54:20 +0300

    Fix: expand RatingSection to fill width

--- presentation/src/main/java/com/moscow/cineverse/screen/details/common/RatingSection.kt
+++ presentation/src/main/java/com/moscow/cineverse/screen/details/common/RatingSection.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -101,7 +102,7 @@ fun RatingSection(
                     color = Theme.colors.shade.secondary
                 )
             }
-
+            Spacer(modifier = Modifier.weight(1f))
             IconButton(onClick = onClick) {
                 Icon(
                     painter = painterResource(R.drawable.outline_alt_arrow_right),
